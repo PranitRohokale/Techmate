f11a7948d7c7ba3be2acebef56ddfd07ded00eac
PranitRohokale <pranitrohokale@gmail.com>
2022-01-23 17:24:29 +0530

    added seeders

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,19 +2,14 @@ import bcrypt from 'bcryptjs'
 
 const users = [
   {
-    name: 'Admin User',
-    email: 'admin@example.com',
+    name: 'Admin',
+    email: 'admin@gmail.com',
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
   {
-    name: 'John Doe',
-    email: 'john@example.com',
-    password: bcrypt.hashSync('123456', 10),
-  },
-  {
-    name: 'Jane Doe',
-    email: 'jane@example.com',
+    name: 'user',
+    email: 'user@gmail.com',
     password: bcrypt.hashSync('123456', 10),
   },
 ]
