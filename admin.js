8437244aa5b1db455fa4951d9f44c9ed860f64b1
PranitRohokale <pranitrohokale@gmail.com>
2022-01-28 00:22:05 +0530

    updated seeders

---
+++ admin.js
@@ -0,0 +1,94 @@
+const admin = [
+  {
+    username: "Admin Account",
+    email: "admin@gmail.com",
+    role: "admin",
+    password: "123456",
+  },
+  {
+    username: "Publisher Account",
+    email: "publisher@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "User Account",
+    email: "user@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "John Doe",
+    email: "john@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "Kevin Smith",
+    email: "kevin@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "Mary Williams",
+    email: "mary@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "Sasha Ryan",
+    email: "sasha@gmail.com",
+    role: "admin",
+    password: "123456",
+  },
+  {
+    username: "Greg Harris",
+    email: "greg@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "Derek Glover",
+    email: "derek@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "Stephanie Hanson",
+    email: "steph@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "Jerry Wiliams",
+    email: "jerry@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "Maggie Johnson",
+    email: "maggie@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "Barry Dickens",
+    email: "barry@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "Ryan Bolin",
+    email: "ryan@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+  {
+    username: "Sara Kensing",
+    email: "sara@gmail.com",
+    role: "user",
+    password: "123456",
+  },
+]
+
+export default admin
