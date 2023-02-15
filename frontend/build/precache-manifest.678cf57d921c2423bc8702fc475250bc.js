5391ec1a890cc60ba2bb705c15ae39349fef438b
PranitRohokale <pranitrohokale@gmail.com>
2023-02-15 12:45:21 +0530

    build ready!

---
+++ frontend/build/precache-manifest.678cf57d921c2423bc8702fc475250bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcea800a320392c31b36696533038f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dfc15f01e1106c843ce",
+    "url": "/static/css/main.f7160af9.chunk.css"
+  },
+  {
+    "revision": "97e9bab916ffda3fb517",
+    "url": "/static/js/2.8dbddcd2.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.8dbddcd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dfc15f01e1106c843ce",
+    "url": "/static/js/main.a92dcd56.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
